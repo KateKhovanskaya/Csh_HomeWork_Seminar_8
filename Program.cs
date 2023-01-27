f9b9425bb429b74ace698a7bcbbc600cdcd7e059
Kate <KhovanskayaK@yandex.ru>
2023-01-27 10:21:20 +0300

    обавлена задача 56

--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
 ﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию
 //элементы каждой строки двумерного массива
-int[,] arr54= GetArray(4,5,0,10);
+int[,] arr54= GetArray(4,3,0,10);
 PrintArray(arr54);
 Console.WriteLine();
-SortArr(arr54);
-PrintArray(arr54);
+//SortArr(arr54);
+//PrintArray(arr54);
+Console.WriteLine($"Минимальная сумма элементов в строке: {FindMinRow(arr54)}");
 
 //-------Методы----------
 int[,] GetArray(int m, int n, int minValue, int maxValue){
@@ -40,4 +41,26 @@ void SortArr(int[,] arr){
             sortSize--;
         }
     }
+}
+
+//Задача 56: Задайте прямоугольный двумерный массив. Напишите программу,
+//которая будет находить строку с наименьшей суммой элементов.
+
+int FindMinRow(int[,] arr){
+    int minRow=0;
+    int min=0;
+    for(int j=0; j<arr.GetLength(1); j++){
+        min+=arr[0,j];
+    }
+    for(int i=1; i<arr.GetLength(0); i++){
+        int sum=0;
+        for(int j=0; j<arr.GetLength(1); j++){
+            sum+= arr[i,j];
+        }
+        if (sum<min){
+            min=sum;
+            minRow=i;
+        }
+    }
+    return minRow+1;
 }
